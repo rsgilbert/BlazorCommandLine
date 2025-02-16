4ab3acd985cb7ed4824ceca77de21ac29a49330f
Gilbert Ssenyonjo <gssenyonjo@gmail.com>
2025-02-16 20:42:56 +0300

    looked at page routing

--- Components/Pages/Counter.razor.cs
+++ Components/Pages/Counter.razor.cs
@@ -7,12 +7,21 @@ namespace BlazorCommandLine.Components.Pages
         private int currentCount = 0;
 
         [Parameter]
+        public int InitialCount {get;set;}
+
+        [Parameter]
         public int IncrementAmount { get;set; } = 1;
 
         private void IncrementCount()
         {
             Console.WriteLine("Incrementing count");
             currentCount += IncrementAmount;
+        }
+
+
+        protected override void OnInitialized()
+        {
+            currentCount = InitialCount;
         }
     }
 }
